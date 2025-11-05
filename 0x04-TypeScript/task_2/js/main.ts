8a70194130eafe975f53e7a17a336977ef43af64
homemix <kenmutati@gmail.com>
2025-11-05 12:44:06 +0300

    ADD: String literal types.

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -63,3 +63,17 @@ function executeWork(employee: Teacher | Director): string {
 
 console.log(executeWork(createEmployee(200)));
 console.log(executeWork(createEmployee(1000)));
+
+
+type Subjects = "Math" | "History";
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === "Math") {
+        return "Teaching Math"
+    }
+    else if (todayClass === "History"){
+        return "Teaching History"
+    }
+}
+
+teachClass("Math")
+
