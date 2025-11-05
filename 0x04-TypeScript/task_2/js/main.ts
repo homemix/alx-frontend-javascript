e4cc374f99303d8ba458e99125e740966baa645d
homemix <kenmutati@gmail.com>
2025-11-05 12:16:56 +0300

    ADD: Creating functions specific to employees.

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -48,3 +48,18 @@ function  createEmployee(salary: number | string): Director | Teacher {
 }
 
 console.log(createEmployee(200));
+
+function isDirector(employee: Teacher | Director): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Teacher | Director): string {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
