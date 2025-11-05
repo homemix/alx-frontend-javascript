03c121b66d5699a73e1b87a88017abe97b25cc60
homemix <kenmutati@gmail.com>
2025-11-05 12:07:16 +0300

    ADD: Advanced types Part 1.

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,50 @@
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+
+class Director implements DirectorInterface{
+    getCoffeeBreak(): string {
+        return "Getting a coffee break";
+    }
+
+    workDirectorTasks(): string {
+        return "Getting to director tasks";
+    }
+
+    workFromHome(): string {
+        return "Working from home";
+    }
+}
+
+class Teacher implements TeacherInterface{
+    getCoffeeBreak(): string {
+        return "Cannot have a break";
+    }
+
+    workFromHome(): string {
+        return "Cannot work from home";
+    }
+
+    workTeacherTasks(): string {
+        return "Getting to work";
+    }
+}
+
+function  createEmployee(salary: number | string): Director | Teacher {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    } else {
+        return new Director();
+    }
+}
+
+console.log(createEmployee(200));
